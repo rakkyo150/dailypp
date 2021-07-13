233e8e34f22f22dca36e1bb8023705ffcfe37d63
rakkyo150 <yoshimura150@hotmail.co.jp>
2021-07-13 10:31:45 +0900

    セッション実装(仮)

--- player_info_db_handler.py
+++ player_info_db_handler.py
@@ -8,11 +8,9 @@ class player_info_db_handler:
         cur = conn.cursor()
         cur.execute('CREATE TABLE IF NOT EXISTS playerInfo(score_saber_url TEXT,oauth_token TEXT,oauth_token_secret TEXT)')
 
-    def scoresaber_info_insert(self,url):
-        cur.execute('INSERT INTO playerInfo VALUES(score_saber_url)',url)
 
-    def tokens_info_insert(self,oauth_token,oauth_token_secret):
-        cur.execute('INSERT INTO playerInfo VALUES(oauth_token,oauth_token_secret)',(oauth_token,oauth_token_secret))
+    def player_info_insert(self,url,oauth_token,oauth_token_secret):
+        cur.execute('INSERT INTO playerInfo VALUES(?,?,?)',(rel,oauth_token,oauth_token_secret))
 
         conn.commit()
         conn.close()
