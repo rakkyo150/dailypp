418f771c5bc2bac267ba1caaaf24f935091bb251
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2021-07-15 02:23:28 +0900

    バグの修正とシークレットキーを環境変数へ設定

--- send_tweet.py
+++ send_tweet.py
@@ -1,10 +1,8 @@
 import tweepy
-
-import secret
-
+import os
 
 def send_tweet(tweet_sentence, token, token_secret):
-    auth = tweepy.OAuthHandler(secret.consumer_key, secret.consumer_secret, )
+    auth = tweepy.OAuthHandler(os.environ['consumer_key'], os.environ['consumer_secret'], )
     auth.set_access_token(token, token_secret)
     api = tweepy.API(auth)
     return api.update_status(tweet_sentence, wait_on_rate_limit=True)
