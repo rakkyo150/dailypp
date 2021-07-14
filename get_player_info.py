cc304e4e161fb1f95899e47ad6c3a34e9be5bd3e
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2021-07-14 18:43:05 +0900

    クリーンアップ

--- get_player_info.py
+++ get_player_info.py
@@ -1,85 +1,91 @@
 from urllib.parse import parse_qsl
-from flask import Flask,jsonify,request,render_template,redirect,session
+
+from flask import Flask, request, render_template, redirect, session
 from requests_oauthlib import OAuth1Session
+
 import secret
 from player_info_db_handler import player_info_db_handler
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX Rnaxaixaixai~XHH!jmN]LWX/,?RT'
 
-base_url="https://api.twitter.com/"
+base_url = "https://api.twitter.com/"
 
-request_token_url=base_url+"oauth/request_token"
-authenticate_url=base_url+"oauth/authenticate"
-access_token_url=base_url+"oauth/access_token"
+request_token_url = base_url + "oauth/request_token"
+authenticate_url = base_url + "oauth/authenticate"
+access_token_url = base_url + "oauth/access_token"
 
-@app.route('/',methods=['GET'])
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
-@app.route('/scoresaber_register_form',methods=['GET'])
+
+@app.route('/scoresaber_register_form', methods=['GET'])
 def register_form():
     return render_template("index2.html")
 
-@app.route('/scoresaber_register_execute',methods=['POST'])
+
+@app.route('/scoresaber_register_execute', methods=['POST'])
 def register_execute():
-    session["url"]=request.form["url"]
-    return render_template("index3.html",verification=session["url"])
+    session["url"] = request.form["url"]
+    return render_template("index3.html", verification=session["url"])
 
-#twitterの連携確認画面作成
-@app.route('/twitter/request_token',methods=['GET'])
+
+# twitterの連携確認画面作成
+@app.route('/twitter/request_token', methods=['GET'])
 def get_twitter_request_token():
+    # Twitter Application Managementで設定したURLを使う
+    oauth_callback = request.args.get('http://127.0.0.1:50000/callback/twitter.html')
 
-    #Twitter Application Managementで設定したURLを使う
-    oauth_callback=request.args.get('http://127.0.0.1:50000/callback/twitter.html')
+    twitter = OAuth1Session(secret.consumer_key, secret.consumer_secret)
 
-    twitter=OAuth1Session(secret.consumer_key,secret.consumer_secret)
-
-    response=twitter.post(
+    response = twitter.post(
         request_token_url,
-        params={"oauth_callback":oauth_callback}
+        params={"oauth_callback": oauth_callback}
     )
 
-    request_token=dict(parse_qsl((response.content.decode("utf-8"))))
+    request_token = dict(parse_qsl((response.content.decode("utf-8"))))
 
-    authenticate_endpoint="%s?oauth_token=%s"\
-        %(authenticate_url,request_token["oauth_token"])
+    authenticate_endpoint = "%s?oauth_token=%s" \
+                            % (authenticate_url, request_token["oauth_token"])
 
-    request_token.update({"authenticate_endpoint":authenticate_endpoint})
+    request_token.update({"authenticate_endpoint": authenticate_endpoint})
 
     print(request_token)
 
     return redirect(request_token["authenticate_endpoint"])
 
-#連携後
-@app.route('/callback/twitter.html',methods=['GET'])
+
+# 連携後
+@app.route('/callback/twitter.html', methods=['GET'])
 def get_twitter_access_token():
-     oauth_token=request.args.get("oauth_token")
-     oauth_verifier=request.args.get("oauth_verifier")
+    oauth_token = request.args.get("oauth_token")
+    oauth_verifier = request.args.get("oauth_verifier")
 
-     twitter=OAuth1Session(
-         secret.consumer_key,
-         secret.consumer_secret,
-         oauth_token,
-         oauth_verifier,
-     )
+    twitter = OAuth1Session(
+        secret.consumer_key,
+        secret.consumer_secret,
+        oauth_token,
+        oauth_verifier,
+    )
 
-     response=twitter.post(
-         access_token_url,
-         params={"oauth_verifier":oauth_verifier}
-     )
+    response = twitter.post(
+        access_token_url,
+        params={"oauth_verifier": oauth_verifier}
+    )
 
-     access_token=dict(parse_qsl(response.content.decode("utf-8")))
+    access_token = dict(parse_qsl(response.content.decode("utf-8")))
 
-     session["oauth_token"]=access_token["oauth_token"]
-     session["oauth_token_secret"]=access_token["oauth_token_secret"]
+    session["oauth_token"] = access_token["oauth_token"]
+    session["oauth_token_secret"] = access_token["oauth_token_secret"]
 
-     player_info_db=player_info_db_handler()
-     player_info_db.player_info_insert(session["url"],access_token["oauth_token"],access_token["oauth_token_secret"])
-     print(session["url"],access_token["oauth_token"],access_token["oauth_token_secret"])
+    player_info_db = player_info_db_handler()
+    player_info_db.player_info_insert(session["url"], access_token["oauth_token"], access_token["oauth_token_secret"])
+    print(session["url"], access_token["oauth_token"], access_token["oauth_token_secret"])
 
-     return render_template("index4.html")
+    return render_template("index4.html")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(port=50000)
--- player_history_db_handler.py
+++ player_history_db_handler.py
@@ -1,8 +1,9 @@
 import sqlite3
 
+
 class player_history_db_handler:
     def __init__(self):
-        global conn,cur
+        global conn, cur
         conn = sqlite3.connect('playerHistory.db')
         cur = conn.cursor()
         cur.execute('CREATE TABLE IF NOT EXISTS history('
@@ -14,12 +15,12 @@ class player_history_db_handler:
                     'topSong TEXT,topPP INTEGER)'
                     )
 
-    def player_yesterday_data_export(self,name):
+    def player_yesterday_data_export(self, name):
         cur.execute('SELECT max(date),pp,gRanking,lRanking,topSong,topPP from history where user=?', (name,))
         yesterday_data = cur.fetchone()
         return yesterday_data
 
-    def player_today_data_import(self,today_data):
+    def player_today_data_import(self, today_data):
         cur.execute('INSERT INTO history VALUES (datetime("now","localtime"),?,?,?,?,?,?)', (
             today_data[0],
             today_data[1],
