df017ac0a8768718fe1ae722b2c6a09afcbd45a0
rakkyo150 <yoshimura150@hotmail.co.jp>
2021-07-15 05:10:51 +0900

    スコアセイバーとtwitterのアカウントの区別 データベースでrecent_playを追加する修正 Twitter連携が切れたときにツイート作成をせずデータベースのデータを削除して終了

--- send_tweet.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-import os
-
-def send_tweet(tweet_sentence, token, token_secret):
-    auth = tweepy.OAuthHandler(os.environ['consumer_key'], os.environ['consumer_secret'], )
-    auth.set_access_token(token, token_secret)
-    api = tweepy.API(auth)
-    return api.update_status(tweet_sentence, wait_on_rate_limit=True)
