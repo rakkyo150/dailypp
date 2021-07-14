74bff184ad963d11c4ee32157bcc31e65f8c609c
rakkyo150 <yoshimura150@hotmail.co.jp>
2021-07-14 18:43:05 +0900

    クリーンアップ

--- player_info_db_handler.py
+++ player_info_db_handler.py
@@ -1,16 +1,17 @@
 import sqlite3
 
-#scoresaberのURLもここで格納されます。
+
+# scoresaberのURLもここで格納されます。
 class player_info_db_handler:
     def __init__(self):
-        global conn,cur
+        global conn, cur
         conn = sqlite3.connect("player_info.db")
         cur = conn.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS playerInfo(score_saber_url TEXT,oauth_token TEXT,oauth_token_secret TEXT)')
+        cur.execute(
+            'CREATE TABLE IF NOT EXISTS playerInfo(score_saber_url TEXT,oauth_token TEXT,oauth_token_secret TEXT)')
 
-
-    def player_info_insert(self,url,oauth_token,oauth_token_secret):
-        cur.execute('INSERT INTO playerInfo VALUES(?,?,?)',(url,oauth_token,oauth_token_secret))
+    def player_info_insert(self, url, oauth_token, oauth_token_secret):
+        cur.execute('INSERT INTO playerInfo VALUES(?,?,?)', (url, oauth_token, oauth_token_secret))
 
         conn.commit()
         conn.close()
