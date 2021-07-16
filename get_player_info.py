b406517f9a21b313056d5038b3b414a2789e6749
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2021-07-16 17:22:06 +0900

    Update get_player_info.py

--- get_player_info.py
+++ get_player_info.py
@@ -92,5 +92,5 @@ def get_twitter_access_token():
 
 
 if __name__ == "__main__":
-    port=int(os.environ.get("PORT",5000)
+    port=int(os.environ.get("PORT",5000))
     app.run(host='0.0.0.0',port=port)
